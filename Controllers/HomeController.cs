f8ccad883db2f2440317942b91794e4edd08a6cc
hkirfan <hkirfan@hsc.wvu.edu>
2019-11-12 11:51:22 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using netcore_vuejs.Models;
+
+namespace netcore_vuejs.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult SubmitedForm([FromBody] FormViewModel form) 
+        {
+            string message = "";
+
+            // Insert to database, send email, etc
+
+            return Json(new { message });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
